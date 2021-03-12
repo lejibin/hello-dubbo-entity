dd0b3f070c5498e327497b0c7476940c3a5c3ba3
Richard <richard@cnheri.com>
2021-03-12 14:00:34 +0800

    Initial commit

---
+++ src/main/java/com/binhow/entity/User.java
@@ -0,0 +1,25 @@
+package com.binhow.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private int userId;
+    private String userName;
+    private Boolean userGender;
+    private String userPassword;
+    @JsonFormat(pattern = "yyyy/MM/dd HH:mm:ss")
+    private Date userBirth;
+    private String userTel;
+    private String userEmail;
+    private String userPhoto;
+    @JsonFormat(pattern = "yyyy/MM/dd HH:mm:ss")
+    private Date userRegistrationTime;
+}
